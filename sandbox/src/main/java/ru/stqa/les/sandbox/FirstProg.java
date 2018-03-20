f9fd04d32b2e8b7d9c1908e7c58aa350fc430dd8
Anastasia <dagnysw@ya.ru>
2018-03-20 15:19:30 +0300

    расчет расстояния между точками

--- sandbox/src/main/java/ru/stqa/les/sandbox/FirstProg.java
+++ sandbox/src/main/java/ru/stqa/les/sandbox/FirstProg.java
@@ -4,8 +4,24 @@ public class FirstProg {
 
     public static void main(String[] args) {
 
-        System.out.println("This is my first program.");
+       // System.out.println("This is my first program.");
+
+        Point p1 = new Point();
+
+        p1.x = 2;
+        p1.y = 3;
+
+        Point p2 = new Point();
+
+        p2.x = 4;
+        p2.y = 6;
 
     }
 
+
+    public static double distance(Point p1, Point p2) {
+
+        return Math.sqrt(Math.pow((p2.x-p1.x),2)+Math.pow((p2.y-p1.y),2));
+
+    }
 }
